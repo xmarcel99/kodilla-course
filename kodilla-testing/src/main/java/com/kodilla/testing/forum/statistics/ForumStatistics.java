f5c02abb5aac9d32897fb53e619781b23d03810d
Marcel Sztur <sensiss99@gmail.com>
2019-03-16 16:45:18 +0100

    Exercises 6.5

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -0,0 +1,38 @@
+package com.kodilla.testing.forum.statistics;
+
+public class ForumStatistics {
+
+    int usersCount;
+    int postCount;
+    int commentsCount;
+    double averagePostForUser;
+    double averageCommentForUser;
+    double averageCommentsForPost;
+    Statistics statistics;
+
+
+
+    public void calculateAdvStatistics(Statistics statistics) {
+
+        usersCount = statistics.usersNames().size();
+        postCount = statistics.postsCount();
+        commentsCount = statistics.commentsCount();
+
+        averagePostForUser = postCount / (double) usersCount;
+        averageCommentForUser = commentsCount / (double) usersCount;
+        averageCommentsForPost = commentsCount / (double) postCount;
+
+
+    }
+
+    public void showStatistics() {
+
+        System.out.println("Users count: " + usersCount);
+        System.out.println("Post count: " + postCount);
+        System.out.println("Comments count: " + commentsCount);
+        System.out.println("Average post for user: " + averagePostForUser);
+        System.out.println("Average comments for user: " + averageCommentForUser);
+        System.out.println("Average comments for post: " + averageCommentsForPost);
+    }
+
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuites.java
@@ -0,0 +1,86 @@
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ForumStatisticsTestSuites {
+
+    @Test
+    public void testCalculateWhenPostCountZeroAndCommentCountZero() {
+
+        //Given
+        Statistics statistics = mock(Statistics.class);
+        ForumStatistics forumStatistics = new ForumStatistics();
+        when(statistics.commentsCount()).thenReturn(0);
+        when(statistics.postsCount()).thenReturn(0);
+        List<String> usersList = new ArrayList<>(Arrays.asList("Marcel"));
+        when(statistics.usersNames()).thenReturn(usersList);
+
+        //When
+        forumStatistics.calculateAdvStatistics(statistics);
+        double resultAverageCommentForUser = forumStatistics.averageCommentForUser;
+        double resultAveragePostForUser = forumStatistics.averagePostForUser;
+
+
+        //Then
+        Assert.assertEquals(0.0,resultAveragePostForUser,0.001);
+         Assert.assertEquals(0.0,resultAverageCommentForUser,0.001);
+    }
+
+    @Test
+    public void testCalculateWhenCommentCountSmallerThenPostCountAndUsersCount100() {
+
+        //Given
+        Statistics statistics = mock(Statistics.class);
+        ForumStatistics forumStatistics = new ForumStatistics();
+        List<String> users = new ArrayList<>();
+        for(int i = 0; i < 100; i ++) {
+            users.add("user"+i);
+        }
+        when(statistics.usersNames()).thenReturn(users);
+        when(statistics.commentsCount()).thenReturn(50);
+        when(statistics.postsCount()).thenReturn(100);
+
+        //When
+        forumStatistics.calculateAdvStatistics(statistics);
+        double resultAverageCommentForPost = forumStatistics.averageCommentsForPost;
+        double resultAveragePostForUser =forumStatistics.averagePostForUser;
+        double resultAverageCommentForUser= forumStatistics.averageCommentForUser;
+
+        //Then
+        Assert.assertEquals(0.5,resultAverageCommentForPost,0.001);
+        Assert.assertEquals(1,resultAveragePostForUser,0.001);
+        Assert.assertEquals(0.5,resultAverageCommentForUser,0.001);
+    }
+
+    @Test
+    public void testCalculateWhenCommentsCountBiggerThanPostCountAndUsersCountZero() {
+
+        //Given
+        Statistics statistics = mock(Statistics.class);
+        ForumStatistics forumStatistics = new ForumStatistics();
+        List<String> users = new ArrayList<>();
+        when(statistics.usersNames()).thenReturn(users);
+        when(statistics.postsCount()).thenReturn(50);
+        when(statistics.commentsCount()).thenReturn(100);
+
+        //When
+        forumStatistics.calculateAdvStatistics(statistics);
+        double resultAverageCommentForPost = forumStatistics.averageCommentsForPost;
+
+        //Then
+        Assert.assertEquals(2,resultAverageCommentForPost,0.001);
+
+
+    }
+
+
+}
