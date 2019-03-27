8acfdaf4df6ebbca753a07d23d2b4cbd8c8cc34d
Marcel Sztur <sensiss99@gmail.com>
2019-03-27 13:04:56 +0100

    nullError

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -1,9 +1,15 @@
 package com.kodilla.exception.io;
 
+import java.io.File;
+
 public class FileReader {
 
     public  void readFile() {
 
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("file/names.txt").getFile());
+
+        System.out.println(file.getPath());
 
     }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -7,7 +7,6 @@ public class ExceptionModuleRunner {
     public static void main (String[] args) {
 
         FileReader fileReader = new FileReader();
-
         fileReader.readFile();
     }
 }
