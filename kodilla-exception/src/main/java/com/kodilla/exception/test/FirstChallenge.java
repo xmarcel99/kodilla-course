5752d89959eb683cc75b22eebc54dbbe80e442c6
Marcel Sztur <sensiss99@gmail.com>
2019-03-27 20:34:48 +0100

    Exercises 8.2 v2

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -2,29 +2,12 @@ package com.kodilla.exception.test;
 
 public class FirstChallenge {
 
-    double result;
-
     public double divide(double a, double b) throws ArithmeticException {
-
-
-        try {
-
-            if(b == 0){
-                throw new ArithmeticException();
-            } else {
-                result = a / b;
-            }
-
-        } catch (ArithmeticException e) {
-
-            System.out.println("You can't devide by 0 !  " + e);
-        } finally {
-            System.out.println("Remember maths is the most important thing in the word !");
+        if(b == 0){
+            throw new ArithmeticException();
         }
-        return result;
+        return a / b;
     }
-
-
 
     /**
      * This main can throw an ArithmeticException!!!
@@ -34,9 +17,14 @@ public class FirstChallenge {
 
         FirstChallenge firstChallenge = new FirstChallenge();
 
-        double result = firstChallenge.divide(3, 0);
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
 
-        System.out.println(result);
-
+        } catch (ArithmeticException e) {
+            System.out.println("Something went wrong: " + e);
+        } finally {
+            System.out.println("Remember maths is the most important thing in word");
+        }
     }
 }
