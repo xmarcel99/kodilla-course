de4c46d8ed2f6f9cfff957cec09ebb209d4aa38f
Marcel Sztur <sensiss99@gmail.com>
2019-03-27 19:54:26 +0100

    Exercises 8.2 v1

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,20 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+
+    public static  void main (String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+
+        try  {
+
+            secondChallenge.probablyIWillThrowException(2,5);
+
+        } catch(Exception e) {
+            System.out.println("Exception: " + e);
+        } finally {
+            System.out.println("Program works");
+        }
+    }
+}
