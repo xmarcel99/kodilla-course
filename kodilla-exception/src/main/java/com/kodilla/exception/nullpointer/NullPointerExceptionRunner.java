0898aa6577137063aad00d90615fe0a07b1a6518
Marcel Sztur <sensiss99@gmail.com>
2019-03-28 13:48:32 +0100

    Exercises 8.4 v1

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/nullpointer/NullPointerExceptionRunner.java
@@ -0,0 +1,20 @@
+package com.kodilla.exception.nullpointer;
+
+public class NullPointerExceptionRunner {
+
+    public static void main (String[] args) {
+
+        User user = null;
+        MessageSender messageSender = new MessageSender();
+
+
+        try {
+            messageSender.sendMessageTo(user,"Test message");
+        } catch (MessageNotSentException e) {
+
+            System.out.println("Ups.... something went wrong: " + e);
+        } finally {
+            System.out.println("Phones are good");
+        }
+    }
+}
